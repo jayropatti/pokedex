23f303fa346f16412f5992212c547ea237ed6362
jayropatti <jayro.pattimaipau@student.hu.nl>
2022-06-10 00:08:41 +0200

    commit html

--- pokedex.js
+++ pokedex.js
@@ -1,8 +1,9 @@
-const pokemonCount = 300;
-var pokedex = {}; // {1 : {"name" : "bulbsaur", "img" : url, "type" : ["grass", "poison"], "desc" : "...."} }
 
-window.onload = async function() {
-    // getPokemon(1);
+const pokemonCount = 151;
+var pokedex = {}; // {1 : {"name" : "bulbasaur", "img" : url, "type" : ["grass", "poison"], "desc" : "...."   }}
+
+window.onload = async function(){
+    //getPokemon(1);
     for (let i = 1; i <= pokemonCount; i++) {
         await getPokemon(i);
         //<div id="1" class="pokemon-name">BULBASAUR</div>
@@ -19,46 +20,41 @@ window.onload = async function() {
     console.log(pokedex);
 }
 
-async function getPokemon(num) {
+async function getPokemon(num){
     let url = "https://pokeapi.co/api/v2/pokemon/" + num.toString();
-
-    let res = await fetch(url);
+    let res = await fetch(url); //wacht tot deze code eindigt en ga dan verder met de code 
     let pokemon = await res.json();
-    // console.log(pokemon);
+    //console.log(pokemon);
 
     let pokemonName = pokemon["name"];
-    let pokemonType = pokemon["types"];
-    let pokemonImg = pokemon["sprites"]["front_default"];
+    let pokemonType = pokemon["type"];
+    let pokemonImage = pokemon["sprites"]["front_default"]; 
 
     res = await fetch(pokemon["species"]["url"]);
     let pokemonDesc = await res.json();
+    //console.log(pokemonDesc); 
 
-    // console.log(pokemonDesc);
-    pokemonDesc = pokemonDesc["flavor_text_entries"][9]["flavor_text"];
+    pokemonDesc = pokemonDesc["flavor_text_entries"][9]["flavor_text"]
 
-    pokedex[num] = {"name" : pokemonName, "img" : pokemonImg, "types" : pokemonType, "desc" : pokemonDesc};
-
+    pokedex[num] = {"name" : pokemonName, "img" : pokemonImage, "types" : pokemonType, "desc" : pokemonDesc}
 }
 
 function updatePokemon(){
-    document.getElementById("pokemon-img").src = pokedex[this.id]["img"];
-
-    //clear previous type
+    document.getElementById("pokemon-img").src= pokedex[this.id]["img"]
+    //verwijder vorige types
     let typesDiv = document.getElementById("pokemon-types");
-    while (typesDiv.firstChild) {
+    while (typesDiv.firstChild){ //als deze div tags bevat, worden deze verwijderd
         typesDiv.firstChild.remove();
     }
-
+    
     //update types
     let types = pokedex[this.id]["types"];
-    for (let i = 0; i < types.length; i++) {
+    for (let i = 0; i < types.length; i++){
         let type = document.createElement("span");
         type.innerText = types[i]["type"]["name"].toUpperCase();
         type.classList.add("type-box");
-        type.classList.add(types[i]["type"]["name"]); //adds background color and font color
+        type.classList.add(types[i]["type"]["name"]); //voegt achtergrond kleur en lettertype kleur toe
         typesDiv.append(type);
     }
 
-    //update description
-    document.getElementById("pokemon-description").innerText = pokedex[this.id]["desc"];
 }
