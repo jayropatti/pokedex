8c7c46ef21c8fd5907191febe48e983324e10be7
jayropatti <jayro.pattimaipau@student.hu.nl>
2022-06-09 21:56:31 +0200

    commit api

---
+++ pokedex.js
@@ -0,0 +1,15 @@
+
+const pokemonCount = 151;
+var pokedex = {}; // {1 : {"name" : "bulbasaur", "img" : url, "type" : ["grass", "poison"], "desc" : "...."   }}
+
+window.onload = function(){
+    getPokemon(1);
+}
+
+function getPokemon(num){
+    let url = "https://pokeapi.co/api/v2/pokemon" + num.toString();
+    let res = fetch(url);
+    let pokemon = res.json;
+    console.log(pokemon);
+
+}
